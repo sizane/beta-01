a4be9fbb57daa0e748d7bcab3750127b62dabb8f
sizane <zane0zero@gmail.com>
2026-04-16 18:10:48 +0700

    update

--- player/entity.java
+++ player/entity.java
@@ -1,10 +1,14 @@
 package player;
 
+import java.awt.image.BufferedImage;
 
 public class entity {
     public int x,y;
     public int speed;
 
-    
+    public BufferedImage pic1,pic2,pic3,pic4;
+    public String direction;
 
+    public int movement = 0;
+    public int movementNum = 1;
 }
--- player/player1.java
+++ player/player1.java
@@ -1,7 +1,12 @@
 package player;
 
-import java.awt.Color;
+//import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import main01.Keyhand;
 import main01.p01;
@@ -22,6 +27,18 @@ public class player1 extends entity{
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage() {
+        try {
+            pic1 = ImageIO.read(getClass().getResourceAsStream("/asset/pic1.png"));
+            pic2 = ImageIO.read(getClass().getResourceAsStream("/asset/pic2.png"));
+            pic3 = ImageIO.read(getClass().getResourceAsStream("/asset/pic3.png"));
+            pic4 = ImageIO.read(getClass().getResourceAsStream("/asset/pic4.png"));
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
     }
     public void update() {
         if (keyH.upPress == true) {
@@ -33,11 +50,44 @@ public class player1 extends entity{
         }else if (keyH.rightPress == true) {
             x += speed;
         }
+
+        movement++;
+        if (movement > 10){
+            if (movementNum == 1) {
+                movementNum = 2;
+            }
+            if (movementNum == 2) {
+                movementNum = 3;
+            }
+            if (movementNum == 3) {
+                movementNum = 4;
+            }
+            movement = 0;
+        }
     }
     public void draw(Graphics2D g2){
-        g2.setColor(Color.BLUE);
+        //g2.setColor(Color.BLUE);
 
-        g2.fillRect (x, y, gp.tileSize, gp.tileSize);
+        //g2.fillRect (x, y, gp.tileSize, gp.tileSize);
 
+        BufferedImage image = null;
+
+        switch(direction) {
+            case "down" :
+            if (movementNum == 1) {
+                image = pic1;
+            }
+            if (movementNum == 2) {
+                image = pic2;
+            }
+            if (movementNum == 3) {
+                image = pic3;
+            }
+            if (movementNum == 4) {
+                image = pic4;
+            }
+        break;
+        }
     }
+       
 }
